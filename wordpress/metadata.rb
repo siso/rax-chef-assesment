57b748ec1c124e66d5ec36e7d851a3f596c7c922
gramos <gramosbr@gmail.com>
2014-12-06 11:16:51 +1100

    Initial structure

---
+++ wordpress/metadata.rb
@@ -0,0 +1,8 @@
+name             'wordpress'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures wordpress'
+long_description 'Installs/Configures wordpress'
+version          '0.1.0'
+
