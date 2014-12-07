bf45eb6cf2bbd22822a75c5d9a696932070a2b76
gramos <gramosbr@gmail.com>
2014-12-07 22:32:54 +1100

    Add mysql configuration

--- wordpress/recipes/mysql.rb
+++ wordpress/recipes/mysql.rb
@@ -6,3 +6,16 @@
 
 include_recipe "mysql::client"
 include_recipe "mysql::server"
+
+node.set['mysql']['server_root_password'] = 'yolo'
+node.set['mysql']['port'] = '3308'
+node.set['mysql']['data_dir'] = '/data'
+
+include_recipe 'mysql::server'
+
+template '/etc/mysql/conf.d/mysite.cnf' do
+  owner 'mysql'
+  owner 'mysql'      
+  source 'mysite.cnf.erb'
+  notifies :restart, 'mysql_service[default]'
+end
